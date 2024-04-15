f3644f98a6769aa80b1a7ceea8cca92ef376368d
Xoneris <kely42@gmx.ch>
2024-04-15 20:00:31 +0200

    made the most basic version of a randomizer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { strategems } from "./strategems";
+import { useState } from "react";
+
 
 function App() {
+
+  const [rando, setRando] = useState(null);
+
+  const randomizer = () => {
+    let strategem_copy = [...strategems];
+    for (let i = strategem_copy.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = strategem_copy[i];
+      strategem_copy[i] = strategem_copy[j];
+      strategem_copy[j] = temp;
+      setRando([...strategem_copy]);
+    }
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <main>
+        <h2>Offensive</h2>
+        <div className="offensive-strategems-comtainer">
+          {strategems.filter((strategem) => strategem.category === "Offensive" ) .map(strategem => (
+              <div>
+                <img src={strategem.icon} alt={strategem.name} />
+              </div>
+          ))}
+        </div>
+        <h2>Supply</h2>
+        <div className="supply-strategems-comtainer">
+          {strategems.filter((strategem) => strategem.category === "Supply" ) .map(strategem => (
+              <div>
+                <img src={strategem.icon} alt={strategem.name} />
+              </div>
+          ))}
+        </div>
+        <h2>Defensive</h2>
+        <div className="defensive-strategems-comtainer">
+          {strategems.filter((strategem) => strategem.category === "Defensive" ) .map(strategem => (
+              <div>
+                <img src={strategem.icon} alt={strategem.name} />
+              </div>
+          ))}
+        </div>
+
+        <button onClick={randomizer}>Randomize</button>
+        <hr/>
+
+        <div>
+          {
+            rando !== null ? 
+            // rando.map(strategem => (
+            //   <img src={strategem.icon} alt={strategem.name} key={strategem.name} />
+            // )):
+            rando.slice(0,4).map((strategem) => (
+              <img src={strategem.icon} alt={strategem.name} />
+              // <span>{strategem.name}</span>
+            )):
+            null
+          }
+
+        </div>
+      </main>
+    </body>
   );
 }
 
---
+++ src/strategems.js
@@ -0,0 +1,254 @@
+
+export const strategems = [
+    {
+      "name": "Anti-Materiel Rifle",
+      "icon": "./strategems/Anti-Materiel Rifle.svg",
+      "category": "Supply"
+    },
+    {
+        "name": "Anti-Personnel Minefield",
+        "icon": "./strategems/Anti-Personnel Minefield.svg",
+        "category": "Defensive"
+    },
+    {
+        "name": "Arc Thrower",
+        "icon": "./strategems/Arc Thrower.svg",
+        "category": "Supply"
+    },
+    {
+        "name": "Autocannon Sentry",
+        "icon": "./strategems/Autocannon Sentry.svg",
+        "category": "Defensive"
+    },
+    {
+        "name": "Autocannon",
+        "icon": "./strategems/Autocannon.svg",
+        "category": "Supply"
+    },
+    {
+        "name": "Ballistic Shield Backpack",
+        "icon": "./strategems/Ballistic Shield Backpack.svg",
+        "category": "Supply"
+    },
+    {
+        "name": "Eagle 110MM Rocket Pods",
+        "icon": "./strategems/Eagle 110MM Rocket Pods.svg",
+        "category": "Offensive"
+    },
+    {
+        "name": "Eagle 500KG Bomb",
+        "icon": "./strategems/Eagle 500KG Bomb.svg",
+        "category": "Offensive"
+    },
+    {
+        "name": "Eagle Airstrike",
+        "icon": "./strategems/Eagle Airstrike.svg",
+        "category": "Offensive"
+    },
+    {
+        "name": "Eagle Cluster Bomb",
+        "icon": "./strategems/Eagle Cluster Bomb.svg",
+        "category": "Offensive"
+    },
+    {
+        "name": "Eagle Napalm Airstrike",
+        "icon": "./strategems/Eagle Napalm Airstrike.svg",
+        "category": "Offensive"
+    },
+    {
+        "name": "Eagle Smoke Strike",
+        "icon": "./strategems/Eagle Smoke Strike.svg",
+        "category": "Offensive"
+    },
+    {
+        "name": "Eagle Strafing Run",
+        "icon": "./strategems/Eagle Strafing Run.svg",
+        "category": "Offensive"
+    },
+    {
+        "name": "EMS Mortar Sentry",
+        "icon": "./strategems/EMS Mortar Sentry.svg",
+        "category": "Defensive"
+    },
+    {
+        "name": "Expendable Anti-Tank",
+        "icon": "./strategems/Expendable Anti-Tank.svg",
+        "category": "Supply"
+    },
+    {
+        "name": "Flamethrower",
+        "icon": "./strategems/Flamethrower.svg",
+        "category": "Supply"
+    },
+    {
+        "name": "Gatling Sentry",
+        "icon": "./strategems/Gatling Sentry.svg",
+        "category": "Defensive"
+    },
+    {
+        "name": "Grenade Launcher",
+        "icon": "./strategems/Grenade Launcher.svg",
+        "category": "Supply"
+    },
+    {
+        "name": "Guard Dog Rover",
+        "icon": "./strategems/Guard Dog Rover.svg",
+        "category": "Supply"
+    },
+    {
+        "name": "Guard Dog",
+        "icon": "./strategems/Guard Dog.svg",
+        "category": "Supply"
+    },
+    {
+        "name": "Heavy Machine Gun",
+        "icon": "./strategems/Heavy Machine Gun.svg",
+        "category": "Supply"
+    },
+    {
+        "name": "HMG Emplacement",
+        "icon": "./strategems/HMG Emplacement.svg",
+        "category": "Defensive"
+    },
+    {
+        "name": "Incendiary Mines",
+        "icon": "./strategems/Incendiary Mines.svg",
+        "category": "Defensive"
+    },
+    {
+        "name": "Jump Pack",
+        "icon": "./strategems/Jump Pack.svg",
+        "category": "Supply"
+    },
+    {
+        "name": "Laser Cannon",
+        "icon": "./strategems/Laser Cannon.svg",
+        "category": "Supply"
+    },
+    {
+        "name": "Machine Gun Sentry",
+        "icon": "./strategems/Machine Gun Sentry.svg",
+        "category": "Defensive"
+    },
+    {
+        "name": "Machine Gun",
+        "icon": "./strategems/Machine Gun.svg",
+        "category": "Supply"
+    },
+    {
+        "name": "Mortar Sentry",
+        "icon": "./strategems/Mortar Sentry.svg",
+        "category": "Defensive"
+    },
+    {
+        "name": "Orbital 120MM HE Barrage",
+        "icon": "./strategems/Orbital 120MM HE Barrage.svg",
+        "category": "Offensive"
+    },
+    {
+        "name": "Orbital 380MM HE Barrage",
+        "icon": "./strategems/Orbital 380MM HE Barrage.svg",
+        "category": "Offensive"
+    },
+    {
+        "name": "Orbital Airburst Strike",
+        "icon": "./strategems/Orbital Airburst Strike.svg",
+        "category": "Offensive"
+    },
+    {
+        "name": "Orbital EMS Strike",
+        "icon": "./strategems/Orbital EMS Strike.svg",
+        "category": "Offensive"
+    },
+    {
+        "name": "Orbital Gas Strike",
+        "icon": "./strategems/Orbital Gas Strike.svg",
+        "category": "Offensive"
+    },
+    {
+        "name": "Orbital Gatling Barrage",
+        "icon": "./strategems/Orbital Gatling Barrage.svg",
+        "category": "Offensive"
+    },
+    {
+        "name": "Orbital Laser",
+        "icon": "./strategems/Orbital Laser.svg",
+        "category": "Offensive"
+    },
+    {
+        "name": "Orbital Precision Strike",
+        "icon": "./strategems/Orbital Precision Strike.svg",
+        "category": "Offensive"
+    },
+    {
+        "name": "Orbital Railcannon Strike",
+        "icon": "./strategems/Orbital Railcannon Strike.svg",
+        "category": "Offensive"
+    },
+    {
+        "name": "Orbital Smoke Strike",
+        "icon": "./strategems/Orbital Smoke Strike.svg",
+        "category": "Offensive"
+    },
+    {
+        "name": "Orbital Walking Barrage",
+        "icon": "./strategems/Orbital Walking Barrage.svg",
+        "category": "Offensive"
+    },
+    {
+        "name": "Patriot Exosuit",
+        "icon": "./strategems/Patriot Exosuit.svg",
+        "category": "Supply"
+    },
+    {
+        "name": "Quasar Cannon",
+        "icon": "./strategems/Quasar Cannon.svg",
+        "category": "Supply"
+    },
+    {
+        "name": "Railgun",
+        "icon": "./strategems/Railgun.svg",
+        "category": "Supply"
+    },
+    {
+        "name": "Recoilless Rifle",
+        "icon": "./strategems/Recoilless Rifle.svg",
+        "category": "Supply"
+    },
+    {
+        "name": "Rocket Sentry",
+        "icon": "./strategems/Rocket Sentry.svg",
+        "category": "Defensive"
+    },
+    {
+        "name": "Shield Generator Pack",
+        "icon": "./strategems/Shield Generator Pack.svg",
+        "category": "Supply"
+    },
+    {
+        "name": "Shield Generator Relay",
+        "icon": "./strategems/Shield Generator Relay.svg",
+        "category": "Defensive"
+    },
+    {
+        "name": "Spear",
+        "icon": "./strategems/Spear.svg",
+        "category": "Supply"
+    },
+    {
+        "name": "Stalwart",
+        "icon": "./strategems/Stalwart.svg",
+        "category": "Supply"
+    },
+    {
+        "name": "Supply Pack",
+        "icon": "./strategems/Supply Pack.svg",
+        "category": "Supply"
+    },
+    {
+        "name": "Tesla Tower",
+        "icon": "./strategems/Tesla Tower.svg",
+        "category": "Defensive"
+    },
+    
+  ]
