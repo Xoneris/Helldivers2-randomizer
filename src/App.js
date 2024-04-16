2c625c74bf006f720e9435727b29f6eeb8918c78
Xoneris <kely42@gmx.ch>
2024-04-16 05:48:50 +0200

    started implementing some options for the randomizer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import { strategems } from "./strategems";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 
 function App() {
 
   const [rando, setRando] = useState(null);
+  const [strategemNumber, setStrategemNumber] = useState(4);
+  const [disableStrategem, setDisableStrategem] = useState([]);
+
+  let onlyOneBackpack = useRef(false);
+  let onlyOneSupportWeapon = useRef(false); 
+
+  let strategem_copy = [...strategems];
 
   const randomizer = () => {
-    let strategem_copy = [...strategems];
+    if (disableStrategem !== null) {
+      strategem_copy = strategems.filter(stratagem => !disableStrategem.includes(stratagem.name));
+    }
+    
+
     for (let i = strategem_copy.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
       let temp = strategem_copy[i];
@@ -17,53 +28,117 @@ function App() {
     }
   }
 
-
+  // const disableEnable = (strategem) => {
+  //   if (disableStrategem.includes(strategem.name)) {
+  //     const index = disableStrategem.indexOf(strategem.name);
+  //     setDisableStrategem(disableStrategem.filter(strats => strats.name !== strategem.name))
+  //   } else {
+  //     setDisableStrategem([...disableStrategem, strategem])
+  //   }
+  // }
+  const disableEnable = (strategemName) => {
+    if (disableStrategem.includes(strategemName)) {
+      const index = disableStrategem.indexOf(strategemName);
+      setDisableStrategem(disableStrategem.filter(strats => strats !== strategemName))
+    } else {
+      setDisableStrategem([...disableStrategem, strategemName])
+    }
+  }
 
   return (
     <body>
       <main>
-        <h2>Offensive</h2>
-        <div className="offensive-strategems-comtainer">
-          {strategems.filter((strategem) => strategem.category === "Offensive" ) .map(strategem => (
-              <div>
-                <img src={strategem.icon} alt={strategem.name} />
-              </div>
-          ))}
+        <div className="wrapper">
+          <section>
+            <h2>Offensive</h2>
+            <div className="offensive-strategems-comtainer">
+              {strategems.filter((strategem) => strategem.category === "Offensive" ) .map(strategem => (
+                  <div>
+                    <img src={strategem.icon} alt={strategem.name} key={strategem.name} onClick={() => {disableEnable(strategem.name)}}
+                      className={disableStrategem.includes(strategem.name) ? "disabled" : null}
+                    />
+                  </div>
+              ))}
+            </div>
+            <h2>Supply</h2>
+            <div className="supply-strategems-comtainer">
+              {strategems.filter((strategem) => strategem.category === "Supply" && strategem.backpack !== true ) .map(strategem => (
+                  <div>
+                    <img src={strategem.icon} alt={strategem.name} key={strategem.name} onClick={() => {disableEnable(strategem.name)}}
+                      className={disableStrategem.includes(strategem.name) ? "disabled" : null}
+                    />
+                  </div>
+              ))}
+              <hr/>
+              {strategems.filter((strategem) => strategem.category === "Supply" && strategem.backpack === true ) .map(strategem => (
+                  <div>
+                    <img src={strategem.icon} alt={strategem.name} key={strategem.name} onClick={() => {disableEnable(strategem.name)}}
+                      className={disableStrategem.includes(strategem.name) ? "disabled" : null}
+                    />
+                  </div>
+              ))}
+            </div>
+            <h2>Defensive</h2>
+            <div className="defensive-strategems-comtainer">
+              {strategems.filter((strategem) => strategem.category === "Defensive" ) .map(strategem => (
+                  <div>
+                    <img src={strategem.icon} alt={strategem.name} key={strategem.name} onClick={() => {disableEnable(strategem.name)}}
+                      className={disableStrategem.includes(strategem.name) ? "disabled" : null}
+                    />
+                  </div>
+              ))}
+            </div>
+          </section>
+          <div>
+            <h2>Options</h2>
+            <fieldset>
+              <legend>Available Strategems</legend>
+                <input type="radio" name="strategem-number" value="3" onClick={() => {setStrategemNumber(3)}}/>
+                <label>3</label>
+                <br/> 
+                <input type="radio" name="strategem-number" value="4" onClick={() => {setStrategemNumber(4)}}/>
+                <label>4</label>
+            </fieldset>
+            <fieldset>
+              <legend>Disable/Enable all Stratagems</legend>
+                <button onClick={() => {setDisableStrategem([])}}>Enable All</button>
+                <button onClick={() => {setDisableStrategem([])}}>Disable All</button>
+            </fieldset>
+            <fieldset>
+              <legend>Options</legend>
+              <input type="checkbox" value="backpack" ref={onlyOneBackpack} onClick={() => onlyOneBackpack.current = !onlyOneBackpack.current} />
+              <label>Only 1 Backpack - {onlyOneBackpack.current === true ? "true" : "false"}</label>
+              <br/>
+              <input type="checkbox" value="support-weapon" ref={onlyOneSupportWeapon} onClick={() => onlyOneSupportWeapon.current = !onlyOneSupportWeapon.current} />
+              <label>Only 1 Support Weapon - {onlyOneSupportWeapon.current  === true ? "true" : "false"}</label>
+            </fieldset>
+          </div>
         </div>
-        <h2>Supply</h2>
-        <div className="supply-strategems-comtainer">
-          {strategems.filter((strategem) => strategem.category === "Supply" ) .map(strategem => (
-              <div>
-                <img src={strategem.icon} alt={strategem.name} />
-              </div>
-          ))}
-        </div>
-        <h2>Defensive</h2>
-        <div className="defensive-strategems-comtainer">
-          {strategems.filter((strategem) => strategem.category === "Defensive" ) .map(strategem => (
-              <div>
-                <img src={strategem.icon} alt={strategem.name} />
-              </div>
-          ))}
-        </div>
-
-        <button onClick={randomizer}>Randomize</button>
-        <hr/>
-
+        
         <div>
-          {
-            rando !== null ? 
-            // rando.map(strategem => (
-            //   <img src={strategem.icon} alt={strategem.name} key={strategem.name} />
-            // )):
-            rando.slice(0,4).map((strategem) => (
-              <img src={strategem.icon} alt={strategem.name} />
-              // <span>{strategem.name}</span>
-            )):
-            null
-          }
+          <button onClick={randomizer}>Randomize</button>
+          <hr/>
 
+          <div>
+            {
+              rando !== null ? 
+              // rando.map(strategem => (
+              //   <img src={strategem.icon} alt={strategem.name} key={strategem.name} />
+              // )):
+              rando.slice(0,strategemNumber).map((strategem) => (
+                <>
+                  {/* <p>Support Weapon: {String(onlyOneSupportWeapon.current)}</p>
+                  <p>Backpack: {String(onlyOneBackpack.current)}</p> */}
+                  <img src={strategem.icon} alt={strategem.name} />
+                </>
+              )):
+              null
+            }
+
+          </div>
         </div>
+
+        
       </main>
     </body>
   );
--- src/strategems.js
+++ src/strategems.js
@@ -3,252 +3,352 @@ export const strategems = [
     {
       "name": "Anti-Materiel Rifle",
       "icon": "./strategems/Anti-Materiel Rifle.svg",
-      "category": "Supply"
+      "category": "Supply",
+      "backpack": false,
+      "support-weapon": true,
     },
     {
         "name": "Anti-Personnel Minefield",
         "icon": "./strategems/Anti-Personnel Minefield.svg",
-        "category": "Defensive"
+        "category": "Defensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Arc Thrower",
         "icon": "./strategems/Arc Thrower.svg",
-        "category": "Supply"
+        "category": "Supply",
+        "backpack": false,
+        "support-weapon": true,
     },
     {
         "name": "Autocannon Sentry",
         "icon": "./strategems/Autocannon Sentry.svg",
-        "category": "Defensive"
+        "category": "Defensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Autocannon",
         "icon": "./strategems/Autocannon.svg",
-        "category": "Supply"
+        "category": "Supply",
+        "backpack": true,
+        "support-weapon": true,
     },
     {
         "name": "Ballistic Shield Backpack",
         "icon": "./strategems/Ballistic Shield Backpack.svg",
-        "category": "Supply"
+        "category": "Supply",
+        "backpack": true,
+        "support-weapon": false,
     },
     {
         "name": "Eagle 110MM Rocket Pods",
         "icon": "./strategems/Eagle 110MM Rocket Pods.svg",
-        "category": "Offensive"
+        "category": "Offensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Eagle 500KG Bomb",
         "icon": "./strategems/Eagle 500KG Bomb.svg",
-        "category": "Offensive"
+        "category": "Offensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Eagle Airstrike",
         "icon": "./strategems/Eagle Airstrike.svg",
-        "category": "Offensive"
+        "category": "Offensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Eagle Cluster Bomb",
         "icon": "./strategems/Eagle Cluster Bomb.svg",
-        "category": "Offensive"
+        "category": "Offensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Eagle Napalm Airstrike",
         "icon": "./strategems/Eagle Napalm Airstrike.svg",
-        "category": "Offensive"
+        "category": "Offensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Eagle Smoke Strike",
         "icon": "./strategems/Eagle Smoke Strike.svg",
-        "category": "Offensive"
+        "category": "Offensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Eagle Strafing Run",
         "icon": "./strategems/Eagle Strafing Run.svg",
-        "category": "Offensive"
+        "category": "Offensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "EMS Mortar Sentry",
         "icon": "./strategems/EMS Mortar Sentry.svg",
-        "category": "Defensive"
+        "category": "Defensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Expendable Anti-Tank",
         "icon": "./strategems/Expendable Anti-Tank.svg",
-        "category": "Supply"
+        "category": "Supply",
+        "backpack": false,
+        "support-weapon": true,
     },
     {
         "name": "Flamethrower",
         "icon": "./strategems/Flamethrower.svg",
-        "category": "Supply"
+        "category": "Supply",
+        "backpack": false,
+        "support-weapon": true,
     },
     {
         "name": "Gatling Sentry",
         "icon": "./strategems/Gatling Sentry.svg",
-        "category": "Defensive"
+        "category": "Defensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Grenade Launcher",
         "icon": "./strategems/Grenade Launcher.svg",
-        "category": "Supply"
+        "category": "Supply",
+        "backpack": false,
+        "support-weapon": true,
     },
     {
         "name": "Guard Dog Rover",
         "icon": "./strategems/Guard Dog Rover.svg",
-        "category": "Supply"
+        "category": "Supply",
+        "backpack": true,
+        "support-weapon": false,
     },
     {
         "name": "Guard Dog",
         "icon": "./strategems/Guard Dog.svg",
-        "category": "Supply"
+        "category": "Supply",
+        "backpack": true,
+        "support-weapon": false,
     },
     {
         "name": "Heavy Machine Gun",
         "icon": "./strategems/Heavy Machine Gun.svg",
-        "category": "Supply"
+        "category": "Supply",
+        "backpack": false,
+        "support-weapon": true,
     },
     {
         "name": "HMG Emplacement",
         "icon": "./strategems/HMG Emplacement.svg",
-        "category": "Defensive"
+        "category": "Defensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Incendiary Mines",
         "icon": "./strategems/Incendiary Mines.svg",
-        "category": "Defensive"
+        "category": "Defensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Jump Pack",
         "icon": "./strategems/Jump Pack.svg",
-        "category": "Supply"
+        "category": "Supply",
+        "backpack": true,
+        "support-weapon": false,
     },
     {
         "name": "Laser Cannon",
         "icon": "./strategems/Laser Cannon.svg",
-        "category": "Supply"
+        "category": "Supply",
+        "backpack": false,
+        "support-weapon": true,
     },
     {
         "name": "Machine Gun Sentry",
         "icon": "./strategems/Machine Gun Sentry.svg",
-        "category": "Defensive"
+        "category": "Defensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Machine Gun",
         "icon": "./strategems/Machine Gun.svg",
-        "category": "Supply"
+        "category": "Supply",
+        "backpack": false,
+        "support-weapon": true,
     },
     {
         "name": "Mortar Sentry",
         "icon": "./strategems/Mortar Sentry.svg",
-        "category": "Defensive"
+        "category": "Defensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Orbital 120MM HE Barrage",
         "icon": "./strategems/Orbital 120MM HE Barrage.svg",
-        "category": "Offensive"
+        "category": "Offensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Orbital 380MM HE Barrage",
         "icon": "./strategems/Orbital 380MM HE Barrage.svg",
-        "category": "Offensive"
+        "category": "Offensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Orbital Airburst Strike",
         "icon": "./strategems/Orbital Airburst Strike.svg",
-        "category": "Offensive"
+        "category": "Offensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Orbital EMS Strike",
         "icon": "./strategems/Orbital EMS Strike.svg",
-        "category": "Offensive"
+        "category": "Offensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Orbital Gas Strike",
         "icon": "./strategems/Orbital Gas Strike.svg",
-        "category": "Offensive"
+        "category": "Offensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Orbital Gatling Barrage",
         "icon": "./strategems/Orbital Gatling Barrage.svg",
-        "category": "Offensive"
+        "category": "Offensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Orbital Laser",
         "icon": "./strategems/Orbital Laser.svg",
-        "category": "Offensive"
+        "category": "Offensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Orbital Precision Strike",
         "icon": "./strategems/Orbital Precision Strike.svg",
-        "category": "Offensive"
+        "category": "Offensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Orbital Railcannon Strike",
         "icon": "./strategems/Orbital Railcannon Strike.svg",
-        "category": "Offensive"
+        "category": "Offensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Orbital Smoke Strike",
         "icon": "./strategems/Orbital Smoke Strike.svg",
-        "category": "Offensive"
+        "category": "Offensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Orbital Walking Barrage",
         "icon": "./strategems/Orbital Walking Barrage.svg",
-        "category": "Offensive"
+        "category": "Offensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Patriot Exosuit",
         "icon": "./strategems/Patriot Exosuit.svg",
-        "category": "Supply"
+        "category": "Supply",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Quasar Cannon",
         "icon": "./strategems/Quasar Cannon.svg",
-        "category": "Supply"
+        "category": "Supply",
+        "backpack": false,
+        "support-weapon": true,
     },
     {
         "name": "Railgun",
         "icon": "./strategems/Railgun.svg",
-        "category": "Supply"
+        "category": "Supply",
+        "backpack": false,
+        "support-weapon": true,
     },
     {
         "name": "Recoilless Rifle",
         "icon": "./strategems/Recoilless Rifle.svg",
-        "category": "Supply"
+        "category": "Supply",
+        "backpack": true,
+        "support-weapon": true,
     },
     {
         "name": "Rocket Sentry",
         "icon": "./strategems/Rocket Sentry.svg",
-        "category": "Defensive"
+        "category": "Defensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Shield Generator Pack",
         "icon": "./strategems/Shield Generator Pack.svg",
-        "category": "Supply"
+        "category": "Supply",
+        "backpack": true,
+        "support-weapon": false,
     },
     {
         "name": "Shield Generator Relay",
         "icon": "./strategems/Shield Generator Relay.svg",
-        "category": "Defensive"
+        "category": "Defensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     {
         "name": "Spear",
         "icon": "./strategems/Spear.svg",
-        "category": "Supply"
+        "category": "Supply",
+        "backpack": true,
+        "support-weapon": true,
     },
     {
         "name": "Stalwart",
         "icon": "./strategems/Stalwart.svg",
-        "category": "Supply"
+        "category": "Supply",
+        "backpack": false,
+        "support-weapon": true,
     },
     {
         "name": "Supply Pack",
         "icon": "./strategems/Supply Pack.svg",
-        "category": "Supply"
+        "category": "Supply",
+        "backpack": true,
+        "support-weapon": false,
     },
     {
         "name": "Tesla Tower",
         "icon": "./strategems/Tesla Tower.svg",
-        "category": "Defensive"
+        "category": "Defensive",
+        "backpack": false,
+        "support-weapon": false,
     },
     
   ]
